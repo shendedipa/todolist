91d42f5fdb1b63fea4a1b9a00c7bfbdcf10e0b95
dipa shende <dipashende016@gmail.com>
2025-03-06 16:41:48 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import Navbar from "./components/navbar/Navbar";
 import Todoo from "./components/Todo/Todo";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Todoo />
     </>
   );
